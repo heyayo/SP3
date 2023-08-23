be4096b49d69a97cc27b3cd36500bbc67e519406
Ray Yang <111838967+oNeko1@users.noreply.github.com>
2023-08-23 18:02:52 +0800

    BossManager

--- Assets/Resources/Items/BossItem.cs
+++ Assets/Resources/Items/BossItem.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "Items/Boss Item")]
 public class BossItem : Item
 {
     [Header("Spawns this boss")]
     [SerializeField]
-    private GameObject bossPrefab;
+    public GameObject bossPrefab;
 
     [SerializeField]
     public string bossName;
 
+    public UnityEvent summon;
+
+    private void OnEnable()
+    {
+        if (summon == null)
+            summon = new UnityEvent();
+    }
+
     public override void Use()
     {
-        Transform playerTransform = PlayerManager.Instance.transform;
-
-        // Spawn Randomly around player
-        float angle = Random.Range(0f, 360f);
-        float x = 20f * Mathf.Cos(angle);
-        float y = 20f * Mathf.Sin(angle);
-        Vector2 spawnPos = new Vector2(x, y);
-
-        Instantiate(bossPrefab, new Vector2(playerTransform.position.x, playerTransform.position.y)
-            + spawnPos, Quaternion.identity);
-
-        consumed.Invoke();
+        if (BossManager.Instance.SummonBoss(bossName))
+            consumed.Invoke();
     }
 }
