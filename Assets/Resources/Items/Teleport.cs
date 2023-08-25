5848d69c0f5ee76a35038a07eabcf626af55eb68
Mrsigna <keegan.zigna@gmail.com>
2023-08-25 23:03:26 +0800

    merge

---
+++ Assets/Resources/Items/Teleport.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : Item
+{
+    private bool isTeleporting = false;
+    private Transform PLAYERPOS;
+    private float teleportCooldown = 2.0f; // Adjust this value as needed
+    private float cooldownTimer = 0.0f;
+
+
+    public override void Use()
+    {
+     
+    }
+
+    public override void WhileHolding()
+    {
+        // Update cooldown timer
+        cooldownTimer -= Time.deltaTime;
+
+        // Check for teleport input if cooldown is not active
+        if (cooldownTimer <= 0.0f && Input.GetMouseButtonDown(0)) // Assuming left mouse button triggers teleport
+        {
+            TeleportToMousePosition();
+            cooldownTimer = teleportCooldown; // Set the cooldown
+        }
+    }
+
+    private void TeleportToMousePosition()
+    {
+        PLAYERPOS =PlayerManager.Instance.transform;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f; // Assuming your game is 2D, adjust if it's 3D
+        PLAYERPOS.position = mousePos;
+
+    }
+
+
+}
