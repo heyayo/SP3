5be483fad80d20b66e2b2ebd0b0e017cedd55922
Ray Yang <111838967+oNeko1@users.noreply.github.com>
2023-08-27 00:02:14 +0800

    DOG Fix

---
+++ Assets/Prefabs/Enemy/Destroyer Of Gods/DOGDamage.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOGDamage : MonoBehaviour
+{
+    private GameObject[] body;
+    private List<Mortality> mortalities = new List<Mortality>();
+
+    void Start()
+    {
+        foreach (Mortality mortality in GetComponentsInChildren<Mortality>())
+        {
+            mortalities.Add(mortality);
+        }
+
+        foreach (GameObject go in GetComponentsInChildren<GameObject>())
+        {
+            go.GetComponent<Damagable>().hit.AddListener(SyncHealth);
+            go.GetComponent<Mortality>().onHealthZero.AddListener(Death);
+        }
+    }
+
+    private void SyncHealth()
+    {
+        foreach (Mortality mortality in mortalities)
+        {
+            mortality.Health = LeastHealth();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    private float LeastHealth()
+    {
+        float lowestHealth = mortalities[0].Health;
+        
+        foreach(Mortality mortality in mortalities)
+        {
+            if (mortality.Health < lowestHealth)
+            {
+                lowestHealth = mortality.Health;
+            }
+        }
+
+        return lowestHealth;
+    }
+}
