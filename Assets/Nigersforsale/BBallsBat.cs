d20d5ba230995a685893de2bbd925d8cf7ee644e
Ray Yang <111838967+oNeko1@users.noreply.github.com>
2023-08-26 23:41:51 +0800

    UI FIX

--- Assets/Nigersforsale/BBallsBat.cs
+++ Assets/Nigersforsale/BBallsBat.cs
@@ -3,7 +3,32 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Items/Baseball")]
 public class BBallsBat : Item
 {
-<<<<<<< HEAD
+    [SerializeField] GameObject BBALLBATPrefab; // Prefab reference
+    GameObject instantiatedBat; // Reference to the instantiated bat
+    bool active;
+
+    public override void Use()
+    {
+        active = !active;
+
+        // Instantiate the bat prefab when it's used for the first time
+        if (instantiatedBat == null && active && BBALLBATPrefab != null)
+        {
+            instantiatedBat = Instantiate(BBALLBATPrefab);
+            //instantiatedBat.SetActive(true);
+        }
+
+        // Toggle the GameObject's active state
+        if (instantiatedBat != null)
+            instantiatedBat.SetActive(active);
+    }
+
+    public override void WhileHolding()
+    {
+        // This function can be empty if the Use function handles activation and deactivation.
+        // You can add additional behavior here if needed.
+    }
+}
 
     //[SerializeField] GameObject BBALLBAT;
     //bool active;
@@ -36,32 +61,3 @@ public class BBallsBat : Item
     //{
     //    ToggleGameObject(active);
     //}
-}
-=======
-    [SerializeField] GameObject BBALLBATPrefab; // Prefab reference
-    GameObject instantiatedBat; // Reference to the instantiated bat
-    bool active;
-
-    public override void Use()
-    {
-        active = !active;
-
-        // Instantiate the bat prefab when it's used for the first time
-        if (instantiatedBat == null && active && BBALLBATPrefab != null)
-        {
-            instantiatedBat = Instantiate(BBALLBATPrefab);
-            //instantiatedBat.SetActive(true);
-        }
-
-        // Toggle the GameObject's active state
-        if (instantiatedBat != null)
-            instantiatedBat.SetActive(active);
-    }
-
-    public override void WhileHolding()
-    {
-        // This function can be empty if the Use function handles activation and deactivation.
-        // You can add additional behavior here if needed.
-    }
-}
->>>>>>> b44ebf81cb9781fb1a06f3a7e3b451ef0dc0b15a
